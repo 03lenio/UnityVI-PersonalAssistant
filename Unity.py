88bf898f12170c77dc2507c39f585fa921005c1c
xliquid808 <54635871+xliquid808@users.noreply.github.com>
2020-12-18 00:36:16 +0100

    ++ (language config for personal assistant)

--- Unity.py
+++ Unity.py
@@ -4,12 +4,37 @@ import datetime
 import wikipedia
 import webbrowser
 import time
+from python_json_config import ConfigBuilder
+# create config parser
+builder = ConfigBuilder()
+
+config = builder.parse_config('config.json')
+lang_file = 0
+language = config.assistant.language
+print(language)
+
+if language == "de":
+    lang_file = builder.parse_config('lang_de.json')
+elif language == "en":
+    lang_file = builder.parse_config('lang.json')
+else:
+    lang_file = builder.parse_config('lang_de.json')
 
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', 'voices[0].id')
-filler_words = ["nach", "bitte", "mir", "suche", "bei", "recherchiere", "zu", "Hey", "Unity"]
+
+if language == "de":
+    engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_DE-DE_ZIRA_11.0')
+elif language == "en":
+    engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+else:
+    engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+
+
+filler_words = lang_file.filler_words
+print(filler_words)
 
 
 def speak(text):
@@ -19,10 +44,10 @@ def speak(text):
 
 def wishMe():
     hour = datetime.datetime.now().hour
-    if hour >= 0 and hour < 12:
+    if 0 <= hour < 12:
         speak("Hi ,Guten Morgen")
         print("Hi ,Guten Morgen")
-    elif hour >= 12 and hour < 16:
+    elif 12 <= hour < 16:
         speak("Guten Nachmittiag")
         print("Guten Nachmittiag")
     else:
@@ -37,11 +62,12 @@ def takeCommand():
         audio = r.listen(source)
 
         try:
-            statement = r.recognize_google(audio, language='de-de')
+            r_language = language
+            statement = r.recognize_google(audio, language=r_language + "-" + r_language)
             print(f"user said:{statement}\n")
 
         except Exception as e:
-            #speak("Keine Ahnung was du meinst")
+            # speak("Keine Ahnung was du meinst")
             return "None"
         return statement
 
@@ -51,7 +77,7 @@ speak("Der Assistent fährt hoch...")
 time.sleep(3)
 speak("Ich bin Unity VI")
 wishMe()
-
+print(lang_file.bye)
 if __name__ == '__main__':
 
     while True:
@@ -60,60 +86,60 @@ if __name__ == '__main__':
         if statement == 0:
             continue
 
-        if "bye" in statement or "tschau" in statement or "ende" in statement:
-            speak('Der Assistent wird beendet,Man sieht sich')
-            print('your personal assistant Unity VI is shutting down,Good bye')
-            break
-        if 'wikipedia' in statement:
-            speak('Searching Wikipedia...')
-            statement = statement.replace("wikipedia", "")
-            results = ""
-            for word in filler_words:
-                if word in statement:
-                    statement = statement.replace(word, "")
-            try:
-                wikipedia.set_lang("de")
-                results = wikipedia.summary(statement, sentences=3)
-            except:
-                speak("Nichts gefunden, sorry")
-                print("Unity understood: " + statement)
-            speak("Nach Wikipedia")
-            print(results)
-            speak(results)
-        elif 'youtube' in statement:
-            webbrowser.open_new_tab("https://www.youtube.com")
-            speak("youtube wird aufgerufen")
-            time.sleep(5)
+        for word in lang_file.bye:
+            if statement in word:
+                print(lang_file.bye)
+                speak('Der Assistent wird beendet,Man sieht sich')
+                print('your personal assistant Unity VI is shutting down,Good bye')
+                break
+                exit()
+        for word in lang_file.wikipedia:
+            if word in statement:
+                speak('Searching Wikipedia...')
+                results = ""
+                for word in filler_words:
+                    if statement in word:
+                        statement = statement.replace(word, "")
+                        print("Vlone:" + statement)
+                try:
+                    wikipedia.set_lang(language)
+                    print(statement)
+                    results = wikipedia.summary(statement, sentences=3)
+                except:
+                    print(language)
+                    print("Vone" + statement)
+                    speak("Nichts gefunden, sorry")
+                    print("Unity understood: " + statement)
+                speak("Nach Wikipedia")
+                print(results)
+                speak(results)
+        for word in lang_file.google:
+                if statement in word:
+                    webbrowser.open_new_tab("https://www.google.com")
+                    speak("Google wird aufgerufen")
+                    time.sleep(5)
 
-        elif 'google' in statement:
-            webbrowser.open_new_tab("https://www.google.com")
-            speak("Google wird aufgerufen")
-            time.sleep(5)
+        for word in lang_file.gmail:
+            if statement in word:
+                webbrowser.open_new_tab("gmail.com")
+                speak("GMail wird aufgerufen")
+                time.sleep(5)
 
-        elif 'gmail' in statement:
-            webbrowser.open_new_tab("gmail.com")
-            speak("GMail wird aufgerufen")
-            time.sleep(5)
-        elif 'stadia' in statement:
-            webbrowser.open_new_tab("https://stadia.google.com/u/2/home")
-            speak("Stadia wird aufgerufen")
-            time.sleep(5)
-        elif 'uhr' in statement:
-            strTime = datetime.datetime.now().strftime("%H:%M")
-            speak(f"es ist gerade {strTime}")
-        elif 'suche' in statement:
-            statement = statement.replace("suche", "")
-            for word in filler_words:
-                if word in statement:
-                    statement = statement.replace(word, "")
-            webbrowser.open_new_tab(statement)
-            time.sleep(5)
+        for word in lang_file.stadia:
+            if statement in word:
+                webbrowser.open_new_tab("https://stadia.google.com/u/2/home")
+                speak("Stadia wird aufgerufen")
+                time.sleep(5)
 
+        for word in lang_file.clock:
+            if statement in word:
+                strTime = datetime.datetime.now().strftime("%H:%M")
+                speak(f"es ist gerade {strTime}")
 
-        else:
-            if statement != "none":
-                speak("Sprich deutsch")
-
-
-
-
+        for word in lang_file.search:
+           if word in statement:
+                for wrd in filler_words:
+                    if wrd in statement:
+                        statement = statement.replace(wrd, "")
+                webbrowser.open_new_tab(statement)
+                time.sleep(5)
